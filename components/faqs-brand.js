be6d3e38b27136d6ffd7a1294b6e217952c2b56c
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-09 00:55:58 +0300

    Google workspace page

--- components/faqs-brand.js
+++ components/faqs-brand.js
@@ -10,19 +10,19 @@ const faqs = [
 
 export default function Example() {
     return (
-        <div className="bg-indigo-700">
+        <div className="bg-blue-700">
             <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-extrabold text-white">
                     Frequently asked questions
                 </h2>
-                <div className="mt-6 border-t border-indigo-300 border-opacity-25 pt-10">
+                <div className="mt-6 border-t border-blue-300 border-opacity-25 pt-10">
                     <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-x-8 md:gap-y-12">
                         {faqs.map((faq) => (
                             <div key={faq.id}>
                                 <dt className="text-lg leading-6 font-medium text-white">
                                     {faq.question}
                                 </dt>
-                                <dd className="mt-2 text-base text-indigo-200">
+                                <dd className="mt-2 text-base text-blue-200">
                                     {faq.answer}
                                 </dd>
                             </div>
