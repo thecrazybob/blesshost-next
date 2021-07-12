191177237da6c7418356b60aace56ee9ff05b4eb
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-12 15:06:17 +0300

    Added image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
+import React from "react";
 import ContactForm from "../components/contact-form";
-import CTAImage from "../components/cta-image";
 import LatestBlog from "../components/latest-blog";
 import Stats from "../components/stats";
 import GradientFeatures from "../components/features-gradient";
 import Testimonials from "../components/testimonials";
-import AlternatingFeature from "../components/features-alternating";
 import Typed from "typed.js";
 import {
     ChatAltIcon,
@@ -16,10 +15,12 @@ import {
     TrashIcon,
     UsersIcon,
 } from "@heroicons/react/outline";
-
+import { SparklesIcon } from "@heroicons/react/outline";
 import { ExternalLinkIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import Image from "next/image";
+import lighthouseImg from "../public/img/lighthouse-100.png";
 
-import React from "react";
 const features = [
     {
         name: "Based in Abu Dhabi, UAE",
@@ -324,7 +325,168 @@ export default function Home() {
                 </div>
             </div>
 
-            <AlternatingFeature></AlternatingFeature>
+            <div className="relative pt-16 pb-32 overflow-hidden">
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100"
+                />
+                <div className="relative">
+                    <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+                            <div>
+                                <div>
+                                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-blue-600 to-blue-800">
+                                        <InboxIcon
+                                            className="h-6 w-6 text-white"
+                                            aria-hidden="true"
+                                        />
+                                    </span>
+                                </div>
+                                <div className="mt-6">
+                                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                                        360 Unlimited Web Hosting
+                                    </h2>
+                                    <p className="mt-4 text-lg text-gray-500">
+                                        The infrastructure of 360 web hosting is
+                                        built on a unique clustered architecture
+                                        that delivers faster, more scalable,
+                                        more reliable hosting service for your
+                                        sites, also our hosting is optimized for
+                                        CMS based websites like WordPress and
+                                        Joomla. Install 70+ popular scripts
+                                        using 1 click.
+                                    </p>
+                                    <div className="mt-6">
+                                        <Link href="/web-hosting">
+                                            <a className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-700">
+                                                Get started
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-8 border-t border-gray-200 pt-6">
+                                <blockquote>
+                                    <div>
+                                        <p className="text-base text-gray-500">
+                                            &ldquo;For a small/medium size
+                                            business the cost is attractive and
+                                            the customer support is awesome.
+                                            When it comes to support, the
+                                            BlessHost team is extremely helpful.
+                                            I am a client of several years and
+                                            give them high marks for all they
+                                            do.&rdquo;
+                                        </p>
+                                    </div>
+                                    <footer className="mt-3">
+                                        <div className="flex items-center space-x-3">
+                                            <div className="flex-shrink-0">
+                                                <img
+                                                    className="h-6 w-6 rounded-full"
+                                                    src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="text-base font-medium text-gray-700">
+                                                Marcia Hill, Digital Marketing
+                                                Manager
+                                            </div>
+                                        </div>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div className="mt-12 sm:mt-16 lg:mt-0">
+                            <div className=" pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                                <img
+                                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                                    src="/img/lighthouse-100.png"
+                                    alt="Customer profile user interface"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-24">
+                    <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+                            <div>
+                                <div>
+                                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-blue-600 to-blue-800">
+                                        <SparklesIcon
+                                            className="h-6 w-6 text-white"
+                                            aria-hidden="true"
+                                        />
+                                    </span>
+                                </div>
+                                <div className="mt-6">
+                                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                                        Website Design & Development
+                                    </h2>
+                                    <p className="mt-4 text-lg text-gray-500">
+                                        Beautiful Websites. Increased
+                                        Conversions. Create a website that best
+                                        represents your business and you are
+                                        going to be proud of. We design and
+                                        develop services specialized in the
+                                        development of stylish modern websites,
+                                        and digital stores for customers around
+                                        the globe. Our Website designers are
+                                        based in Abu Dhabi, UAE. Inquire now to
+                                        get a free quote.
+                                    </p>
+                                    <div className="mt-6">
+                                        <Link href="/website-design">
+                                            <a className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-700">
+                                                Get started
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-8 border-t border-gray-200 pt-6">
+                                <blockquote>
+                                    <div>
+                                        <p className="text-base text-gray-500">
+                                            &ldquo;Brilliant and skilled people
+                                            with extensive web development
+                                            experience in complex e-commerce
+                                            websites. Thanks to their highly
+                                            creative approach that led to
+                                            successful results. &rdquo;
+                                        </p>
+                                    </div>
+                                    <footer className="mt-3">
+                                        <div className="flex items-center space-x-3">
+                                            <div className="flex-shrink-0">
+                                                <img
+                                                    className="h-6 w-6 rounded-full"
+                                                    src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="text-base font-medium text-gray-700">
+                                                Marcia Hill, Digital Marketing
+                                                Manager
+                                            </div>
+                                        </div>
+                                    </footer>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+                            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                                <img
+                                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                                    alt="Customer profile user interface"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <GradientFeatures
                 title="Why should you trust BlessHost?"
