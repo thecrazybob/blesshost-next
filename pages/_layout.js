cdd090ba067dfc140f0158b636931de79a2a7265
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-16 02:43:34 +0300

    SEO page

--- pages/_layout.js
+++ pages/_layout.js
@@ -13,6 +13,7 @@ export default function Layout({ children }) {
                 ></meta>
                 <script
                     src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+                    defer
                 />
                 <link rel="icon" href="/favicon.ico" />
 
