307ca831aea5b2542de618663f2d49840a15b0c5
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-28 22:02:58 +0300

    Added favicon

--- pages/_layout.js
+++ pages/_layout.js
@@ -3,106 +3,103 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Head>
-                <title>Make a website in Dubai!</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                ></meta>
-                <script
-                    src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-                    defer
-                />
-                <link rel="icon" href="/favicon.ico" />
+  return (
+    <>
+      <Head>
+        <title>Make a website in Dubai!</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          defer
+        />
+        <link rel="icon" href="/favicon.ico" />
 
-                {/* Favicon */}
-                <link
-                    rel="apple-touch-icon"
-                    sizes="57x57"
-                    href="/favicon/apple-icon-57x57.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="60x60"
-                    href="/favicon/apple-icon-60x60.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="72x72"
-                    href="/favicon/apple-icon-72x72.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="76x76"
-                    href="/favicon/apple-icon-76x76.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="114x114"
-                    href="/favicon/apple-icon-114x114.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="120x120"
-                    href="/favicon/apple-icon-120x120.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="144x144"
-                    href="/favicon/apple-icon-144x144.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="/favicon/apple-icon-152x152.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/favicon/apple-icon-180x180.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="192x192"
-                    href="/favicon/android-icon-192x192.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="96x96"
-                    href="/favicon/favicon-96x96.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/favicon/manifest.json" />
-                <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta
-                    name="msapplication-TileImage"
-                    content="/ms-icon-144x144.png"
-                />
-                <meta name="theme-color" content="#ffffff"></meta>
+        {/* Favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"></meta>
 
-                <meta name="description" content="Make a website in Dubai" />
-            </Head>
+        <meta name="description" content="Make a website in Dubai" />
+      </Head>
 
-            <Header></Header>
+      <Header></Header>
 
-            {children}
+      {children}
 
-            <Footer></Footer>
-        </>
-    );
+      <Footer></Footer>
+    </>
+  );
 }
