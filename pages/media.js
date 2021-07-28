4c70b301f6fc03b57d7bf4d2e6f3b8c0b57465c5
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-28 13:54:36 +0300

    Media page

--- pages/media.js
+++ pages/media.js
@@ -1,5 +1,176 @@
-export default function Example() {
-    return (
-        <></>
-    )
+import TitleBar from "../components/title-bar";
+
+const colors = [
+  {
+    name: "Bleu de France",
+    hex: "#3e83f8",
+    class: "bg-blesshost-blue",
+  },
+  {
+    name: "Madison",
+    hex: "#2a3e53",
+    class: "bg-blesshost-darkblue",
+  },
+];
+
+const files = [
+  {
+    title: "Logo Light",
+    png: "/img/media-kit/1.png",
+    svg: "/img/media-kit/1.svg",
+    source: "/img/media-kit/1.svg",
+  },
+  {
+    title: "Badge",
+    png: "/img/media-kit/2.png",
+    svg: "/img/media-kit/2.svg",
+    source: "/img/media-kit/2.svg",
+  },
+  {
+    title: "Cloud and BH",
+    png: "/img/media-kit/3.png",
+    svg: "/img/media-kit/3.svg",
+    source: "/img/media-kit/3.svg",
+  },
+  {
+    title: "Text under cloud",
+    png: "/img/media-kit/4.png",
+    svg: "/img/media-kit/4.svg",
+    source: "/img/media-kit/4.svg",
+  },
+  {
+    title: "Logo Dark",
+    png: "/img/media-kit/1a.png",
+    svg: "/img/media-kit/1a.svg",
+    source: "/img/media-kit/1a.svg",
+    dark: true,
+  },
+  {
+    title: "Badge Dark",
+    png: "/img/media-kit/2a.png",
+    svg: "/img/media-kit/2a.svg",
+    source: "/img/media-kit/2a.svg",
+    dark: true,
+  },
+  {
+    title: "Cloud and BH Dark",
+    png: "/img/media-kit/3a.png",
+    svg: "/img/media-kit/3a.svg",
+    source: "/img/media-kit/3a.svg",
+    dark: true,
+  },
+  {
+    title: "Text under cloud Dark",
+    png: "/img/media-kit/4a.png",
+    svg: "/img/media-kit/4a.svg",
+    source: "/img/media-kit/4a.svg",
+    dark: true,
+  },
+  {
+    title: "BH under cloud",
+    png: "/img/media-kit/5.png",
+    svg: "/img/media-kit/5.svg",
+    source: "/img/media-kit/5.svg",
+  },
+  {
+    title: "BH under cloud Dark",
+    png: "/img/media-kit/5a.png",
+    svg: "/img/media-kit/5a.svg",
+    source: "/img/media-kit/5a.svg",
+    dark: true,
+  },
+];
+
+export default function mediaPage() {
+  return (
+    <div className="text-center md:text-left mb-16 lg:mt-0 mx-auto max-w-7xl px-4 sm:px-6">
+      <TitleBar
+        subheading="About BlessHost"
+        title="Media Kit"
+        description="You can find a wide variety of our logos and banners designed to the perfection by the best of our designers."
+      />
+      <ul
+        role="list"
+        className="mt-10 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      >
+        {files.map((file) => (
+          <li key={file.source} className="relative">
+            <div
+              className={` ${
+                file.dark === true ? "bg-gray-800" : "bg-gray-100"
+              } group block w-full aspect-w-10 aspect-h-7 rounded-lg focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-blue-500 overflow-hidden" `}
+            >
+              <img
+                src={file.source}
+                alt={file.title}
+                className="px-6 pointer-events-none group-hover:opacity-75"
+              />
+              <a
+                href={file.source}
+                className="absolute inset-0 focus:outline-none"
+              >
+                <span className="sr-only">View details for {file.title}</span>
+              </a>
+            </div>
+            <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+              {file.title}
+            </p>
+            <p className="block text-sm font-medium text-gray-500">
+              <a
+                download
+                className="hover:border-b border-gray-800 transition-all ease-in-out duration-75"
+                href={file.svg}
+              >
+                SVG
+              </a>{" "}
+              <a
+                download
+                className="hover:border-b border-gray-800 transition-all ease-in-out duration-75"
+                href={file.png}
+              >
+                PNG
+              </a>
+            </p>
+          </li>
+        ))}
+      </ul>
+      <TitleBar
+        title="Brand Colors"
+        description="The amazing colors we use that make up our identity"
+        subheading="Media Kit"
+      />
+      <div className="mt-10 grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {colors.map((color) => (
+          <div
+            key={color.name}
+            className="rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          >
+            <div className="flex-shrink-0">
+              <div className={`h-10 w-10 rounded-full ${color.class}`} />
+            </div>
+            <div className="flex-1 min-w-0">
+              <div className="focus:outline-none">
+                <span className="absolute inset-0" aria-hidden="true" />
+                <p className="text-sm font-medium text-gray-900">
+                  {color.name}
+                </p>
+                <p className="text-sm text-gray-500 truncate uppercase">
+                  {color.hex}
+                </p>
+              </div>
+            </div>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(color.hex);
+              }}
+              type="button"
+              className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Copy to clipboard
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,42 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    mode: "jit",
-    purge: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Gilroy", ...defaultTheme.fontFamily.sans],
-                heading: ["Avenir"],
-            },
-            colors: {
-                teal: colors.teal,
-                cyan: colors.cyan,
-            },
-            height: {
-                192: "48rem",
-                144: "36rem",
-                120: "30rem",
-            },
-            width: {
-                192: "48rem",
-                144: "36rem",
-                120: "30rem",
-            },
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Gilroy", ...defaultTheme.fontFamily.sans],
+        heading: ["Avenir"],
+      },
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+        blesshost: {
+          blue: "#3e83f8",
+          darkblue: "#2a3e53",
         },
+      },
+      height: {
+        192: "48rem",
+        144: "36rem",
+        120: "30rem",
+      },
+      width: {
+        192: "48rem",
+        144: "36rem",
+        120: "30rem",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/typography"),
-    ],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
