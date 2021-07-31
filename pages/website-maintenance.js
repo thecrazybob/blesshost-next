96f5024d053d55161e5c2c59bbb7fa40d25cf61b
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-31 16:18:09 +0300

    Website maintenance pricing

--- pages/website-maintenance.js
+++ pages/website-maintenance.js
@@ -73,6 +73,7 @@ import {
 } from "@heroicons/react/outline";
 
 import Testimonials from "../components/testimonials";
+import { useCurrency } from "../contexts/CurrencyContext";
 
 const faqs = [
   {
@@ -116,6 +117,7 @@ const faqs = [
 const pricing = {
   tiers: [
     {
+      pid: 161,
       title: "Essential",
       frequency: "/month",
       description: "The essentials to provide your best work for clients.",
@@ -130,6 +132,7 @@ const pricing = {
       mostPopular: false,
     },
     {
+      pid: 162,
       title: "Professional",
       frequency: "/month",
       description: "A plan that scales with your rapidly growing business.",
@@ -145,6 +148,7 @@ const pricing = {
       mostPopular: true,
     },
     {
+      pid: 163,
       title: "Business",
       frequency: "/month",
       description: "Dedicated support and infrastructure for your company.",
@@ -161,12 +165,18 @@ const pricing = {
     },
   ],
 };
-
+import { useState } from "react";
+import Pricing from "../components/pricing-three-tier";
+import ContactForm from "../components/contact-form";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Page() {
+export default function websiteMaintenancePage() {
+  const [tier, setTier] = useState(pricing.tiers[0]);
+  const [billingInterval, setBillingInterval] = useState("monthly");
+  const { currency, setCurrency } = useCurrency("");
+
   return (
     <>
       {/* Hero */}
@@ -1425,74 +1435,20 @@ export default function Page() {
         </div>
       </div>
 
-      {/* Pricing */}
-      <div class="bg-white">
-        <div className="max-w-7xl mx-auto py-24 px-4 bg-white sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
-            Pricing plans for teams of all sizes
-          </h2>
-          <p className="mt-6 max-w-2xl text-xl text-gray-500">
-            Choose an affordable plan that's packed with the best features for
-            engaging your audience, creating customer loyalty, and driving
-            sales.
-          </p>
-
-          {/* Tiers */}
-          <div className="mt-24 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
-            {pricing.tiers.map((tier) => (
-              <div
-                key={tier.title}
-                className="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col"
-              >
-                <div className="flex-1">
-                  <h3 className="text-xl font-semibold text-gray-900">
-                    {tier.title}
-                  </h3>
-                  {tier.mostPopular ? (
-                    <p className="absolute top-0 py-1.5 px-4 bg-blue-500 rounded-full text-xs font-semibold uppercase tracking-wide text-white transform -translate-y-1/2">
-                      Most popular
-                    </p>
-                  ) : null}
-                  <p className="mt-4 flex items-baseline text-gray-900">
-                    <span className="text-5xl font-extrabold tracking-tight">
-                      ${tier.price}
-                    </span>
-                    <span className="ml-1 text-xl font-semibold">
-                      {tier.frequency}
-                    </span>
-                  </p>
-                  <p className="mt-6 text-gray-500">{tier.description}</p>
-
-                  {/* Feature list */}
-                  <ul role="list" className="mt-6 space-y-6">
-                    {tier.features.map((feature) => (
-                      <li key={feature} className="flex">
-                        <CheckIcon
-                          className="flex-shrink-0 w-6 h-6 text-blue-500"
-                          aria-hidden="true"
-                        />
-                        <span className="ml-3 text-gray-500">{feature}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-
-                <a
-                  href="#"
-                  className={classNames(
-                    tier.mostPopular
-                      ? "bg-blue-500 text-white hover:bg-blue-600"
-                      : "bg-blue-50 text-blue-700 hover:bg-blue-100",
-                    "mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium"
-                  )}
-                >
-                  {tier.cta}
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Pricing
+        pricing={pricing}
+        tier={tier}
+        setTier={setTier}
+        currency={currency}
+        term={billingInterval}
+      />
+      <ContactForm
+        tier={tier}
+        setTier={setTier}
+        term={billingInterval}
+        currency={currency}
+        plans={pricing.tiers}
+      />
       <Testimonials />
       <FAQSDark faqs={faqs} />
       <CTASimple />
