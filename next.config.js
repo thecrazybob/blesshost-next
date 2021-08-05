8dae947d16e4cddd234ae4dcab29d28e7d6f66be
Abdullah <abdullah77@outlook.com>
2021-08-05 22:43:52 +0500

    revamp pricing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-    images: {
-        domains: [
-            "blog.blesshost.com",
-            "secure.gravatar.com",
-            "images.unsplash.com",
-        ],
-    },
-};
+// next.config.js
+const createNextPluginPreval = require("next-plugin-preval/config");
+const withNextPluginPreval = createNextPluginPreval();
+
+module.exports = withNextPluginPreval({
+  images: {
+    domains: [
+      "blog.blesshost.com",
+      "secure.gravatar.com",
+      "images.unsplash.com",
+    ],
+  },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+});
