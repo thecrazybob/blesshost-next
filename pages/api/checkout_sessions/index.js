d3372d3d235d4cf3c9851ccc55898c2971b20922
Mohammed Sohail <itscrazybob@gmail.com>
2021-08-08 23:56:19 +0300

    wip

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -23,8 +23,7 @@ export default async function handler(req, res) {
 
       res.status(200).json(checkoutSession);
     } catch (err) {
-      console.log(err);
-      // res.status(500).json({ statusCode: 500, message: err.message });
+      res.status(500).json({ statusCode: 500, message: err.message });
     }
   } else {
     res.setHeader("Allow", "POST");
