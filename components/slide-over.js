d1893f1a5242c5636dfed35f45060b9b4d0f1b6e
Mohammed Sohail <itscrazybob@gmail.com>
2021-11-30 15:12:40 +0300

    -m

--- components/slide-over.js
+++ components/slide-over.js
@@ -16,6 +16,7 @@ export default function Checkout({ open, setOpen }) {
   const { currency } = useCurrency();
   const [promoCodeOpen, setPromoCodeOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [addingToCart, setAddingToCart] = useState(false);
 
   let total = 0.0;
 
@@ -302,9 +303,36 @@ export default function Checkout({ open, setOpen }) {
                   <div className="flex-shrink-0 px-4 py-4 flex justify-end">
                     {products.length > 0 ? (
                       <button
-                        onClick={handleCheckout}
+                        onClick={(e) => {
+                          handleCheckout(e);
+                          setAddingToCart(true);
+                        }}
                         className="flex-grow inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                       >
+                        {addingToCart ? (
+                          <svg
+                            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
+                        ) : (
+                          ""
+                        )}
                         Checkout
                       </button>
                     ) : null}
