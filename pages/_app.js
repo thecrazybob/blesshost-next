00b6023fc3321544a71684a1561a5398e47b289c
Mohammed Sohail <hey@msohail.dev>
2022-07-25 16:27:33 +0300

    Added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { CurrencyContextProvider } from "./../contexts/CurrencyContext";
 import { CartContextProvider } from "../contexts/CartContext";
 import { CartOpenContextProvider } from "../contexts/CartOpenContext";
+import Script from "next/script";
 
 import Layout from "../pages/_layout";
 export default function App({ Component, pageProps }) {
@@ -17,6 +18,19 @@ export default function App({ Component, pageProps }) {
       <CartOpenContextProvider>
         <CartContextProvider>
           <Layout>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=GTM-KSS7RD"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'GTM-KSS7RD');
+            `}
+            </Script>
             <Component {...pageProps} />
           </Layout>
         </CartContextProvider>
