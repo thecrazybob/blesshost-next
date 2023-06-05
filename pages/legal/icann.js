efc87bdc759d1d5cc766460580bd71d769e15588
thewasim55 <63788795+thewasim55@users.noreply.github.com>
2023-06-05 17:29:02 +0400

    icann update

--- pages/legal/icann.js
+++ pages/legal/icann.js
@@ -6,15 +6,32 @@ export default function Example() {
       <TitleBar subheading="Legal" title="ICANN" />
       <div>
   <section dir="ltr" external_links="">
-    <h1 className="LibraryIntro">ICANN | Registrants' Benefits and Responsibilities</h1>
+    <h1 className="LibraryIntro">Registrants' Benefits and Responsibilities</h1>
+    <div className="Document--translations">
+      <h3>This page is available in:</h3>
+      <ul>
+        <li>English</li>
+        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-ar">العربية</a></li>
+        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-es">Español</a></li>
+        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-fr">Français</a></li>
+        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-ru">Pусский</a></li>
+        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-zh">中文</a></li>
+      </ul>
+    </div>
     <div className="EmbeddedHTML">
       <div className="box">
         <p className="floatcenter"><a href="/en/resources/registrars/registrant-rights/responsibilities-27jun11-en.htm">View archived policy</a></p>
       </div>
-      <h3><em><abbr title="Domain Name" className="">Domain Name</abbr> Registrants' Rights:</em></h3>
+      <h3>
+        <em>
+          <abbr title="Domain Name" className="">Domain Name</abbr> Registrants' Rights:
+        </em>
+      </h3>
       <ol>
         <li>
-          <p>Your domain name registration and any privacy/proxy services you may use in conjunction with it must be subject to a Registration Agreement with an <abbr title="Internet Corporation for Assigned Names and Numbers">ICANN</abbr> Accredited Registrar.
+          <p>
+            Your domain name registration and any privacy/proxy services you may use in conjunction with it must be subject to a Registration Agreement with an
+            <abbr title="Internet Corporation for Assigned Names and Numbers">ICANN</abbr> Accredited Registrar.
           </p>
           <ul>
             <li>
@@ -49,7 +66,37 @@ export default function Example() {
           </ul>
         </li>
         <li>
-          <p>You shall not be subject to false advertising or deceptive practices by your Registrar or though any proxy
+          <p>You shall not be subject to false advertising or deceptive practices by your Registrar or though any proxy or privacy services made available by your Registrar. This includes deceptive notices, hidden fees, and any practices that are illegal under the consumer protection law of your residence.</p>
+        </li>
+      </ol>
+      <h3>
+        <em>
+          <abbr title="Domain Name" className="">Domain Name</abbr> Registrants' Responsibilities:
+        </em>
+      </h3>
+      <ol>
+        <li>
+          <p>You must comply with the terms and conditions posted by your Registrar, including applicable policies from your Registrar, the Registry and
+            <abbr title="Internet Corporation for Assigned Names and Numbers" className="">ICANN</abbr>.</p>
+        </li>
+        <li>
+          <p>You must review your Registrar's current Registration Agreement, along with any updates.</p>
+        </li>
+        <li>
+          <p>You will assume sole responsibility for the registration and use of your domain name.</p>
+        </li>
+        <li>
+          <p>You must provide accurate information for publication in directories such as
+            <abbr title="WHOIS (pronounced &quot;who is&quot;; not an acronym)" className="">WHOIS</abbr>, and promptly update this to reflect any changes.</p>
+        </li>
+        <li>
+          <p>You must respond to inquiries from your Registrar within fifteen (15) days, and keep your Registrar account data current. If you choose to have your domain name registration renew automatically, you must also keep your payment information current.</p>
+        </li>
+      </ol>
+    </div>
+  </section>
+</div>
+
     </div>
   );
 }
