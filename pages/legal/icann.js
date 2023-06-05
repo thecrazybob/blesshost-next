1ee038cc29f5377b283167349b8bd74f650edb48
thewasim55 <63788795+thewasim55@users.noreply.github.com>
2023-06-05 16:56:35 +0400

    ICANN page added

---
+++ pages/legal/icann.js
@@ -0,0 +1,267 @@
+import TitleBar from "../../components/title-bar";
+
+export default function Example() {
+  return (
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-15 pb-10 translate-exclude">
+      <TitleBar subheading="Legal" title="Acceptable Usage Policy" />
+      <p className="mt-3">
+        As a provider of web site hosting and other Internet-related services,
+        BlessHost offers its customer (also known as "Subscribers") and their
+        customers and users the means to acquire and disseminate a wealth of
+        public, private, commercial and non-commercial information. BlessHost
+        respects that the Internet provides a forum for free and open discussion
+        and dissemination of information. However, when there are competing
+        interests at issue, BlessHost reserves the right to take certain
+        preventive or corrective actions. In order to protect these competing
+        interests, BlessHost has developed an&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Acceptable Use Policy ("AUP")
+        </strong>
+        , which supplements and explains certain terms of each customer's
+        respective service agreement, and is intended as a guide to the
+        customer's rights and obligations when using BlessHost' services. This
+        AUP will be revised from time to time.
+      </p>
+      <p className="mt-3">
+        One important aspect of the Internet is that no one party owns or
+        controls it. This fact accounts for much of the Internet's openness and
+        value, but it also places a high premium on the judgment and
+        responsibility of those who use it, both in the information they acquire
+        and in the information they disseminate to others. When subscribers
+        obtain information through the Internet, they must keep in mind that
+        BlessHost cannot monitor, verify, warrant or vouch for the accuracy and
+        quality of the information they acquire. For this reason, the subscriber
+        must exercise his or her best judgment in relying on information
+        obtained from the Internet, and also should be aware that some material
+        posted to the Internet may be sexually explicit or otherwise offensive.
+        Because BlessHost cannot monitor or censor the Internet, and will not
+        attempt to do so, BlessHost cannot accept any responsibility for injury
+        to its subscribers resulting from inaccurate, unsuitable, offensive or
+        illegal Internet communications.
+      </p>
+      <p className="mt-3">
+        When subscribers disseminate information from the Internet, they must
+        keep in mind that BlessHost does not review, edit, censor or take
+        responsibility for any information its subscribers may create. When
+        users place information on the Internet, they have the same liability as
+        other authors for copyright infringement, defamation and other harmful
+        speech. Also, because the information created is carried over BlessHost'
+        network and may reach a large number of people, including both
+        subscribers and non-subscribers of BlessHost, subscribers' postings to
+        the Internet may affect other subscribers and may affect BlessHost'
+        goodwill, business, reputation or operations. For these reasons,
+        subscribers violate BlessHost policy and the Service Agreement when
+        they, their customers, affiliates or subsidiaries engage in the
+        following prohibited activities:
+      </p>
+      <p className="mt-3">
+        1.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Spamming:
+          <br />
+        </strong>
+        Sending unsolicited bulk and/or commercial information over the
+        Internet. It is not only harmful because of its negative impact on
+        consumer attitudes toward BlessHost, but also because it can overload
+        BlessHost' network and disrupt service to BlessHost' subscribers. Also,
+        maintaining an open SMTP relay is prohibited. When a complaint is
+        received, BlessHost will investigate and shutdown the account that is
+        SPAMing. A $250 charge for violating this policy will be charged to the
+        person initiating the SPAM. Furthermore BlessHost reserves the right to
+        prosecute for this violation. A $1.00 charge will be assessed PER EMAIL
+        sent should BlessHost choose to pursue and prosecute.
+      </p>
+      <p className="mt-3">
+        2.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Adult-Oriented Content:
+          <br />
+        </strong>
+        BlessHost requires to be notified may you wish to provide adult content,
+        whether it be images, video, games, audio or software. Your intent to do
+        so must be stated prior to, or at the time of purchase. Failure to
+        comply may result in your account being suspended/terminated without
+        notice.
+      </p>
+      <p className="mt-3">
+        3.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Obscene Speech or Materials:
+          <br />
+        </strong>
+        Using BlessHost' network to advertise, transmit, store, post, display,
+        or otherwise make available child pornography or obscene speech or
+        material is prohibited. BlessHost is required by law to notify law
+        enforcement agencies when it becomes aware of the presence of child
+        pornography on or being transmitted through its network.
+      </p>
+      <p className="mt-3">
+        4.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Defamatory or Abusive Language:
+          <br />
+        </strong>
+        Using BlessHost' network as a means to transmit or post negative,
+        defamatory, harassing, abusive or threatening language.
+      </p>
+      <p className="mt-3">
+        5.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Forging of Headers:
+          <br />
+        </strong>
+        Forging or misrepresenting message headers, whether in whole or in part,
+        to mask the originator of the message.
+      </p>
+      <p className="mt-3">
+        6.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Illegal or Unauthorized Access to Other Computers or Networks:
+          <br />
+        </strong>
+        Accessing, illegally or without authorization, computers, accounts or
+        networks belonging to another party, or attempting to penetrate security
+        measures of another individual's system (often known as "hacking").
+        Also, any activity that may be used as a precursor to an attempted
+        system penetration (i.e., port scan, stealth scan or other
+        information-gathering activity).
+      </p>
+      <p className="mt-3">
+        7.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Distribution of Internet Viruses, Worms, Trojan Horses or Other
+          Destructive Activities
+        </strong>
+        &nbsp;
+        <strong className="leading-10 border-b text-lg">
+          :<br />
+        </strong>
+        Distributing information regarding the creation of and sending Internet
+        viruses, worms, Trojan horses, pinging, flooding, mail bombing or denial
+        of service attacks. Also, activities that disrupt the use of or
+        interfere with the ability of others to effectively use the network or
+        any connected network, system, service or equipment.
+      </p>
+      <p className="mt-3">
+        8.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Facilitation a Violation of this AUP:
+          <br />
+        </strong>
+        Advertising, transmitting or otherwise making available any software,
+        program, product or service that is designed to violate this AUP, which
+        includes the facilitation of the means to spam, initiation of pinging,
+        flooding, mail bombing, denial of service attacks and piracy of
+        software.
+      </p>
+      <p className="mt-3">
+        9.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Export Control Violations:
+          <br />
+        </strong>
+        Exporting encryption software over the Internet or otherwise, to points
+        outside the United States.
+      </p>
+      <p className="mt-3">
+        10.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Usenet Groups:
+          <br />
+        </strong>
+        BlessHost reserves the right not to accept postings from newsgroups
+        where we have actual knowledge that the content of the newsgroup
+        violates the AUP.
+      </p>
+      <p className="mt-3">
+        11.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Other Illegal Activities:
+          <br />
+        </strong>
+        Engaging in activities that are determined to be illegal, including, but
+        not limited to, advertising, transmitting or otherwise making available
+        Ponzi schemes, pyramid schemes, fraudulently charging credit cards and
+        pirating software.
+      </p>
+      <p className="mt-3">
+        12.&nbsp;
+        <strong className="leading-10 border-b text-lg">
+          Other Activities:
+          <br />
+        </strong>
+        Engaging in activities, whether lawful or unlawful, that BlessHost
+        determines to be harmful to its subscribers, operations, and reputation,
+        goodwill or customer relations.
+      </p>
+      <p className="mt-3">
+        As we have pointed out, the responsibility for avoiding harmful
+        activities just described rests primarily with the subscriber. BlessHost
+        will not, as an ordinary practice, monitor the communications of its
+        subscribers to ensure that comply with BlessHost' policy or applicable
+        law. However, when BlessHost becomes aware of harmful activities, it may
+        take any action to stop the harmful activity, including, but not limited
+        to, removal of information, shutting down a web site, implementing
+        screening software designed to block offending transmissions, denying
+        access to the Internet, or any other action deemed appropriate by
+        BlessHost.
+      </p>
+      <p className="mt-3">
+        BlessHost is also aware that many of its subscribers are themselves
+        providers of Internet services, and that information reaching BlessHost'
+        facilities from those subscribers may have originated from a customer of
+        the subscriber or from another third party. BlessHost does not require
+        its subscribers who offer Internet services to monitor or censor
+        transmissions or web sites created by customers of its subscribers.
+        BlessHost reserves the right to directly take action against a customer
+        of its subscribers. Also, BlessHost may take action against the
+        BlessHost' subscriber because of activities of a customer of the
+        subscriber, even though the action may affect other customers of the
+        subscriber. Similarly, BlessHost anticipates that subscribers who offer
+        Internet services will cooperate with BlessHost in any corrective or
+        preventive action that BlessHost deems necessary. Failure to cooperate
+        with such corrective or preventive measures is a violation of BlessHost
+        policy.
+      </p>
+      <p className="mt-3">
+        BlessHost will not intentionally monitor private electronic mail
+        messages sent or receive by its subscribers, unless required to do so by
+        law, governmental authority or when public safety is at stake. BlessHost
+        may, however, monitor its service electronically to determine that its
+        facilities are operating satisfactorily. Also, BlessHost may disclose
+        information, including, but not limited to, information concerning a
+        subscriber, a transmission made using our network, or a web site, in
+        order to comply with a court order, subpoena, summons, discovery
+        request, warrant, statute, regulation or governmental request. BlessHost
+        assumes no obligation to inform the subscriber that subscriber
+        information has been provided and, in some cases, may be prohibited by
+        law from giving such notice. Finally, BlessHost may disclose subscriber
+        information or information transmitted over its network where necessary
+        to protect BlessHost and others from harm, or where such disclosure is
+        necessary to the proper operation of the system. However, BlessHost will
+        never sell information to other services or outside companies.
+      </p>
+      <p className="mt-3">
+        BlessHost expects that its subscribers who provide Internet services to
+        others will comply fully with all applicable laws concerning the privacy
+        of online communications. A subscriber's failure to comply with those
+        laws will violate BlessHost policy. Finally, BlessHost wishes to
+        emphasize that, in signing the Service Agreement, subscribers indemnify
+        BlessHost for any violation of the Service Agreement, law or BlessHost
+        policy resulting in loss to BlessHost or the bringing of any claim
+        against BlessHost by any third party. This means that, if BlessHost is
+        sued because of a subscriber's or customer of a subscriber's activity,
+        the subscriber will be responsible for payment of any damages awarded
+        against BlessHost, plus costs and reasonable attorney's fees.
+      </p>
+      <p className="mt-3">
+        We hope this AUP is helpful in clarifying the obligations of Internet
+        users, including BlessHost and its subscribers, as responsible members
+        of the Internet. Any complaints about a subscriber's violation of this
+        AUP should be sent to&nbsp;
+        <a href="mailto:abuse@blesshost.com">abuse@blesshost.com</a>.
+      </p>
+      <p></p>
+    </div>
+  );
+}
