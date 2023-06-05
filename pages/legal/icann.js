488dc34fb4182b4261c2a35738f8b7683fd4c9c2
thewasim55 <63788795+thewasim55@users.noreply.github.com>
2023-06-05 17:45:06 +0400

    icann page addition

--- pages/legal/icann.js
+++ pages/legal/icann.js
@@ -5,98 +5,52 @@ export default function Example() {
     <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-15 pb-10 translate-exclude">
       <TitleBar subheading="Legal" title="ICANN" />
       <div>
-  <section dir="ltr" external_links="">
-    <h1 className="LibraryIntro">Registrants' Benefits and Responsibilities</h1>
-    <div className="Document--translations">
-      <h3>This page is available in:</h3>
+  <h2>Registrants' Benefits and Responsibilities (ICANN)</h2>
+  <h3>Domain Name Registrants' Rights:</h3>
+  <ol>
+    <li>
+      <p>Your domain name registration and any privacy/proxy services you may use in conjunction with it must be subject to a Registration Agreement with an ICANN Accredited Registrar.</p>
       <ul>
-        <li>English</li>
-        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-ar">العربية</a></li>
-        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-es">Español</a></li>
-        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-fr">Français</a></li>
-        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-ru">Pусский</a></li>
-        <li className="current"><a href="https://www.icann.org/resources/pages/benefits-2017-10-27-zh">中文</a></li>
+        <li>You are entitled to review this Registration Agreement at any time, and download a copy for your records.</li>
       </ul>
-    </div>
-    <div className="EmbeddedHTML">
-      <div className="box">
-        <p className="floatcenter"><a href="/en/resources/registrars/registrant-rights/responsibilities-27jun11-en.htm">View archived policy</a></p>
-      </div>
-      <h3>
-        <em>
-          <abbr title="Domain Name" className="">Domain Name</abbr> Registrants' Rights:
-        </em>
-      </h3>
-      <ol>
-        <li>
-          <p>
-            Your domain name registration and any privacy/proxy services you may use in conjunction with it must be subject to a Registration Agreement with an
-            <abbr title="Internet Corporation for Assigned Names and Numbers">ICANN</abbr> Accredited Registrar.
-          </p>
-          <ul>
-            <li>
-              <p>You are entitled to review this Registration Agreement at any time, and download a copy for your records.</p>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <p>You are entitled to accurate and accessible information about:</p>
-          <ul>
-            <li>
-              <p>The identity of your <abbr title="Internet Corporation for Assigned Names and Numbers" className="">ICANN</abbr> Accredited Registrar;</p>
-            </li>
-            <li>
-              <p>The identity of any proxy or privacy service provider affiliated with your Registrar;</p>
-            </li>
-            <li>
-              <p>Your Registrar's terms and conditions, including pricing information, applicable to domain name registrations;</p>
-            </li>
-            <li>
-              <p>The terms and conditions, including pricing information, applicable to any privacy services offered by your Registrar;</p>
-            </li>
-            <li>
-              <p>The customer support services offered by your Registrar and the privacy services provider, and how to access them;</p>
-            </li>
-            <li>
-              <p>How to raise concerns and resolve disputes with your Registrar and any privacy services offered by them; and</p>
-            </li>
-            <li>
-              <p>Instructions that explain your Registrar's processes for registering, managing, transferring, renewing, and restoring your domain name registrations, including through any proxy or privacy services made available by your Registrar.</p>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <p>You shall not be subject to false advertising or deceptive practices by your Registrar or though any proxy or privacy services made available by your Registrar. This includes deceptive notices, hidden fees, and any practices that are illegal under the consumer protection law of your residence.</p>
-        </li>
-      </ol>
-      <h3>
-        <em>
-          <abbr title="Domain Name" className="">Domain Name</abbr> Registrants' Responsibilities:
-        </em>
-      </h3>
-      <ol>
-        <li>
-          <p>You must comply with the terms and conditions posted by your Registrar, including applicable policies from your Registrar, the Registry and
-            <abbr title="Internet Corporation for Assigned Names and Numbers" className="">ICANN</abbr>.</p>
-        </li>
-        <li>
-          <p>You must review your Registrar's current Registration Agreement, along with any updates.</p>
-        </li>
-        <li>
-          <p>You will assume sole responsibility for the registration and use of your domain name.</p>
-        </li>
-        <li>
-          <p>You must provide accurate information for publication in directories such as
-            <abbr title="WHOIS (pronounced &quot;who is&quot;; not an acronym)" className="">WHOIS</abbr>, and promptly update this to reflect any changes.</p>
-        </li>
-        <li>
-          <p>You must respond to inquiries from your Registrar within fifteen (15) days, and keep your Registrar account data current. If you choose to have your domain name registration renew automatically, you must also keep your payment information current.</p>
-        </li>
-      </ol>
-    </div>
-  </section>
+    </li>
+    <li>
+      <p>You are entitled to accurate and accessible information about:</p>
+      <ul>
+        <li>The identity of your ICANN Accredited Registrar;</li>
+        <li>The identity of any proxy or privacy service provider affiliated with your Registrar;</li>
+        <li>Your Registrar's terms and conditions, including pricing information, applicable to domain name registrations;</li>
+        <li>The terms and conditions, including pricing information, applicable to any privacy services offered by your Registrar;</li>
+        <li>The customer support services offered by your Registrar and the privacy services provider, and how to access them;</li>
+        <li>How to raise concerns and resolve disputes with your Registrar and any privacy services offered by them; and</li>
+        <li>Instructions that explain your Registrar's processes for registering, managing, transferring, renewing, and restoring your domain name registrations, including through any proxy or privacy services made available by your Registrar.</li>
+      </ul>
+    </li>
+    <li>
+      <p>You shall not be subject to false advertising or deceptive practices by your Registrar or though any proxy or privacy services made available by your Registrar. This includes deceptive notices, hidden fees, and any practices that are illegal under the consumer protection law of your residence.</p>
+    </li>
+  </ol>
+  <h3>Domain Name Registrants' Responsibilities:</h3>
+  <ol>
+    <li>
+      <p>You must comply with the terms and conditions posted by your Registrar, including applicable policies from your Registrar, the Registry and ICANN.</p>
+    </li>
+    <li>
+      <p>You must review your Registrar's current Registration Agreement, along with any updates.</p>
+    </li>
+    <li>
+      <p>You will assume sole responsibility for the registration and use of your domain name.</p>
+    </li>
+    <li>
+      <p>You must provide accurate information for publication in directories such as WHOIS, and promptly update this to reflect any changes.</p>
+    </li>
+    <li>
+      <p>You must respond to inquiries from your Registrar within fifteen (15) days, and keep your Registrar account data current. If you choose to have your domain name registration renew automatically, you must also keep your payment information current.</p>
+    </li>
+  </ol>
 </div>
 
+    
     </div>
   );
 }
